1e8f0b76a62635dae323bfb25b0e2696271b8189
Prabh Khosa <kprabhpreet25@gmail.com>
2026-02-19 21:53:00 +0530

    db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql=require('mysql2/promise');
 require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: process.env.host,
-  user: 'root',
-  password: '2004',
-  database: 'heer'
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 })
 // connection.connect((err=>{
 //   if(err)throw err;
