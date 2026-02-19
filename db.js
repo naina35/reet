968d9e64f6af6ba679472111efd68087e146b89c
Prabh Khosa <kprabhpreet25@gmail.com>
2026-02-19 22:17:14 +0530

    ssl

--- db.js
+++ db.js
@@ -5,7 +5,10 @@ const connection = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
+    ssl: {
+    rejectUnauthorized: false
+  }
 })
 // connection.connect((err=>{
 //   if(err)throw err;
