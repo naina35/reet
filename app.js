459acf15a1ca9287be5bee5f31c1362bc8878941
Prabh Khosa <kprabhpreet25@gmail.com>
2026-02-19 21:58:13 +0530

    deploy changes

--- app.js
+++ app.js
@@ -43,7 +43,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 module.exports = app;
