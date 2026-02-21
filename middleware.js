8cbbbcf53c982592457b5ed9d4e7770443053a0e
Prabh Khosa <kprabhpreet25@gmail.com>
2026-02-21 14:43:14 +0530

    //

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ const jwt= require('jsonwebtoken');
 require('dotenv').config();
 function isAuthenticated(req,res,next){
     const {authorization}=req.headers;
-    console.log(req.headers);
+    //console.log(req.headers);
     if(!authorization){
         return res.status(401).json({
             error:"unauthorized"
@@ -12,7 +12,7 @@ function isAuthenticated(req,res,next){
         try{
             const token=authorization.split(' ')[1];
             const payload=jwt.verify(token,process.env.JWT_ACCESS_SECRET);
-            console.log(payload);
+            //console.log(payload);
             req.payload=payload;
         }
         catch (err){
